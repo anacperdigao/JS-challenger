388ce4dd27643519032abceb4e338288373886fb
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-22 19:10:14 -0300

    exercicio somar dois numeros

---
+++ Javascript-basics/SumTwoNumbers.js
@@ -0,0 +1,21 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function
+myFunction (a, b) {
+
+    return a + b;
+}
+
+console.log(myFunction(1,2))
+console.log(myFunction(1,10))
+console.log(myFunction(99,1))
+
+/* RESPOSTA NODE.JS
+
+Info: Start process (19:09:18)
+3
+11
+100
+Info: End process (19:09:18)
+*/
