5e7509ed1968c4049eb98c887a08cce84d4255ad
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-23 13:12:31 -0300

    Prototype indexOf

---
+++ Javascript-basics/FindThePositionOfOneStringInAnother.js
@@ -0,0 +1,19 @@
+// Write a function that takes a string as argument
+// The string contains the substring 'is'
+// Return the index of 'is'
+function myFunction (a) {
+
+    return console.log(a.indexOf('is'));
+}
+
+myFunction("praise")
+myFunction("risky")
+myFunction("paris")
+
+/* RESULTADO NODE.JS
+Info: Start process (13:10:59)
+3
+1
+3
+Info: End process (13:10:59)
+*/
