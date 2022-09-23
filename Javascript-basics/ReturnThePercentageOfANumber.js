ac82d18b55e5539baeabd2cc6cb5bcfb0167dfc0
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-23 14:13:00 -0300

    porcentagem de um numero

---
+++ Javascript-basics/ReturnThePercentageOfANumber.js
@@ -0,0 +1,20 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+function myFunction (a, b) {
+
+    return console.log(b/100 * a); 
+}
+
+
+myFunction(100,50)
+myFunction(10,1)
+myFunction(500,25)
+
+/* RESULTADO NODE.JS
+
+Info: Start process (14:11:55)
+50
+0.1
+125
+Info: End process (14:11:55)
+*/
