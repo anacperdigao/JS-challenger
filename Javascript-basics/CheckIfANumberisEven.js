62deee399c56ae707a7c2fd536a61f506df151bc
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-24 12:36:30 -0300

    checando numero par

---
+++ Javascript-basics/CheckIfANumberisEven.js
@@ -0,0 +1,22 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+function myFunction (a) {
+
+return console.log(a % 2 === 0);
+}
+
+myFunction(10)
+myFunction(-4)
+myFunction(5)
+myFunction(-111)
+
+/* RESULTADO NODE.JS
+Info: Start process (12:34:32)
+true
+true
+false
+false
+Info: End process (12:34:32)
+
+*/
