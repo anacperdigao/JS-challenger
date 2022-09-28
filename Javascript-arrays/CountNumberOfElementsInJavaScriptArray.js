b6d46df1d2dfbace76bba98916b5abd7d2ec30e2
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-28 18:56:56 -0300

    Exercicios array.length

---
+++ Javascript-arrays/CountNumberOfElementsInJavaScriptArray.js
@@ -0,0 +1,19 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+function myFunction (a) {
+
+    return console.log(a.length);
+}
+
+myFunction([1,2,2,4])
+myFunction([9,9,9])
+myFunction([4,3,2,1,0])
+
+/* RESPOSTA NODE.JS
+
+Info: Start process (18:55:31)
+4
+3
+5
+Info: End process (18:55:31)
+*/
