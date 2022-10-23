bfa684a4d5d6be4e4269d7e43a6d2d4755ade640
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-23 11:03:40 -0300

    check the checkbox

---
+++ Javascript-Dom/CheckTheCheckbox.js
@@ -0,0 +1,11 @@
+//Extend the JavaScript code below to interact with the displayed HTML elements. 
+//Once you click the button, the checkbox should be checked.
+//Confirm your code by clicking the button!
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+  
+    // type in your code here
+  const checkbox = document.getElementById('checkbox');
+  checkbox.checked = true;
+});
