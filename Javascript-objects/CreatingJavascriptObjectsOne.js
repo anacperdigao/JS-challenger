a356a1bf4313a42c78739938169cffcf5b9870d3
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-28 19:00:21 -0300

    Exercicio de objeto

---
+++ Javascript-objects/CreatingJavascriptObjectsOne.js
@@ -0,0 +1,19 @@
+// Write a function that takes a string as argument. 
+// Create an object that has a property with key 'key' and a value equal to the string. Return the object.
+
+
+function myFunction (a) {
+
+    return console.log({key: a})
+}
+
+myFunction('a')
+myFunction('z')
+myFunction('b')
+
+// RESPOSTA NODE.JS
+// Info: Start process (18:59:16)
+// { key: 'a' }
+// { key: 'z' }
+// { key: 'b' }
+// Info: End process (18:59:16)
